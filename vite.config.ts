46a056339a2b5f05039db159d1d5ae52ee3855e7
daiki.s <daiki.s@itnav.co.jp>
2022-10-12 00:31:19 +0900

    fix: all

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,10 @@ import vue from "@vitejs/plugin-vue";
 import path from "path";
 // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
 import vuetify from "vite-plugin-vuetify";
-import { ghPages } from "vite-plugin-gh-pages";
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./",
-  plugins: [vue(), vuetify({ autoImport: true }), ghPages()],
+  base: "/vue3-barcode-scanner-demo/",
+  plugins: [vue(), vuetify({ autoImport: true })],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
